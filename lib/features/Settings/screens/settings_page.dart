ba6736c4d6ca7f6d31ffefb429a722f0b32def14
pnaskardev <pnaskardev@gmail.com>
2023-03-06 16:52:52 +0530

    profile card has been added

--- lib/features/Settings/screens/settings_page.dart
+++ lib/features/Settings/screens/settings_page.dart
@@ -1,6 +1,8 @@
 import 'package:ball_dont_lie/features/Settings/widgets/change_theme_button.dart';
+import 'package:ball_dont_lie/features/Settings/widgets/profile_widget.dart';
 import 'package:ball_dont_lie/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:neopop/widgets/cards/neopop_card.dart';
 import 'package:provider/provider.dart';
 
 class SettingsPage extends StatefulWidget 
@@ -23,14 +25,61 @@ class _SettingsPageState extends State<SettingsPage> with AutomaticKeepAliveClie
     (
       child: Scaffold
       (
+        appBar: AppBar
+        (
+          centerTitle: true,
+          title: const Text('Settings'),
+
+        ),
         body: Center
         (
-          child: ListTile
+          child: Column
           (
-            // leading: Icon(Icons.dark_mode),
-            leading: Provider.of<ThemeProvider>(context).getDarkEnabled ? const Icon(Icons.dark_mode) : const Icon(Icons.light_mode),
-            title: const Text('Change theme'),
-            trailing: const ChangeThemeButton()
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: 
+            [
+              ListTile
+              (
+                // leading: Icon(Icons.dark_mode),
+                leading: Provider.of<ThemeProvider>(context).getDarkEnabled ? const Icon(Icons.dark_mode) : const Icon(Icons.light_mode),
+                title: const Text('Change theme'),
+                trailing: const ChangeThemeButton()
+              ),
+              const ListTile
+              (
+                title: ProfileWidget()
+              ),
+              const ListTile
+              (
+                title: NeoPopCard
+                (
+                  color: Colors.white24,
+                  borderColor:Colors.black,
+                  depth: 10,
+                  hShadowColor: Colors.purpleAccent,
+                  vShadowColor: Colors.deepPurpleAccent,
+                  child: SizedBox
+                  (
+                    height: 100,
+                    child: Center
+                    (
+                      child: Padding
+                      (
+                        padding: EdgeInsets.all(16.0),
+                        child: Text
+                        (
+                          'More features and Settings coming soon',
+                          style: TextStyle
+                          (
+                            color: Colors.blueGrey
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ),
+              )
+            ],
           )
         ),
       ),
